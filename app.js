cbba57de82eb5826456fadabc0cc4b7adb885ff1
LeoCristobal <leocristobal108@gmail.com>
2026-06-13 15:13:16 +0800

    Authentication: Arcjet

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from "express";
 import { API_VERSION1, PORT } from "./config/env.js";
 import authRouter from "./routes/auth/auth.routes.js";
-import connectToDb from "./database/database.js";
 import errorMiddleWare from "./middleware/error.middleware.js";
 import userRouter from "./routes/user/user.routes.js";
 import authMiddleware from "./middleware/auth.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middleware/arcjet.middleware.js";
 const app = express();
 
 // MIDDLEWARE
-app.use([errorMiddleWare, express.json(), cookieParser()]);
+app.use([errorMiddleWare, express.json(), cookieParser(), arcjetMiddleware]);
 
 // ROUTES
 app.use(`${API_VERSION1}/auth`, authRouter);
